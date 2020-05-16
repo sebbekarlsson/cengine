9f347da86ad00cbede5375b1738f862944697857
Sebastian Karlsson <sebbekarlsson97@gmail.com>
2020-05-16 21:42:28 +0200

    adds shells script to download libraries

---
+++ getpackages.sh
@@ -0,0 +1,32 @@
+prevdir=$(pwd)
+
+
+mkdir -p packages
+cd packages
+
+git clone git://github.com/glfw/glfw.git
+cd glfw
+cmake .
+make
+cd ../
+
+git clone git://git.savannah.gnu.org/freetype/freetype2.git
+cd freetype2
+./autogen.sh
+./configure
+make
+cd ../
+
+git clone git://github.com/recp/cglm.git
+cd cglm
+cmake .
+make
+cd ../
+
+wget https://sourceforge.net/projects/glew/files/glew/2.1.0/glew-2.1.0.tgz/download
+tar xf download
+cd glew-2.1.0
+make
+cd ..
+
+cd $prevdir
