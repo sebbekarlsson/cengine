b609779af391abbcf87498f763e6e3f273cb62fb
Sebastian Karlsson <sebbekarlsson97@gmail.com>
2020-05-16 16:49:03 +0200

    use specified color in text fragment shader

--- res/shader/text_fragment.glsl
+++ res/shader/text_fragment.glsl
@@ -1,5 +1,6 @@
 #version 330 core
 in vec2 fragment_texcoord;
+in vec4 fragment_color;
 out vec4 color;
 
 uniform sampler2D u_texture;
@@ -7,5 +8,5 @@ uniform sampler2D u_texture;
 void main()
 {    
     vec4 sampled = vec4(1.0, 1.0, 1.0, texture(u_texture, fragment_texcoord).r);
-    color = vec4(vec3(1, 1, 1), 1.0) * sampled;
+    color = vec4(fragment_color) * sampled;
 }
